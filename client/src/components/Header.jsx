f52cb238f1cfb1ac1f6c116dbae6b57e4e1b75bb
sandking-number1 <Alfred.J9412@gmail.com>
2023-12-20 00:27:45 +0530

    header with nav bar

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,42 @@
+import { Link } from 'react-router-dom'
+
+const Header = () => {
+  return (
+    <header className="px-8 py-4 bg-[#fafafe] shadow-sm">
+      <nav className="flex justify-between">
+        <Link to="/">
+          <h1 className="text-2xl sm:text-xl font-bold">
+            <span className="text-[#7065f0]">LimbePuli</span>
+            <span className="text-slate-700"> Estate</span>
+          </h1>
+        </Link>
+        <div className="flex gap-16 justify-center items-center">
+          <ul className="gap-8 text-[#52586f] font-bold tracking-wide hidden md:flex">
+            <Link to="/">
+              <li>Home</li>
+            </Link>
+            <Link to="/about">
+              <li>About us</li>
+            </Link>
+          </ul>
+          <div className="gap-6 hidden md:flex">
+            <Link to="/sign-in">
+              <button className="border px-6 py-3 border-slate-300 font-bold rounded-lg">
+                Login
+              </button>
+            </Link>
+            <Link to="/sign-up">
+              <button className="bg-[#7065f0]  px-5 py-3 text-white font-bold rounded-lg">
+                Sign up
+              </button>
+            </Link>
+          </div>
+        </div>
+        <div className="md:hidden flex justify-center items-center">
+          hamburger
+        </div>
+      </nav>
+    </header>
+  )
+}
+export default Header
