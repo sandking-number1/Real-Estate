75a6abb82b30b95cea9d9f6d43d3125d44990f09
sandking-number1 <Alfred.J9412@gmail.com>
2023-12-20 23:17:35 +0530

    database connection

--- api/index.js
+++ api/index.js
@@ -1,7 +1,22 @@
+require('dotenv').config()
+
 const express = require('express')
+const connectDB = require('./db/connectDB')
 
 const app = express()
 
-app.listen(3000, () => {
-  console.log('server listeninggA on port 3000....')
-})
+const port = process.env.PORT || 3000
+
+const start = async () => {
+  try {
+    await connectDB(process.env.MONGO_URI)
+    app.listen(3000, () => {
+      console.log('server listening on port 3000....')
+      // realEstateProject
+    })
+  } catch (error) {
+    console.log(error)
+  }
+}
+
+start()
